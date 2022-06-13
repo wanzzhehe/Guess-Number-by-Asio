4321ac9b644daad41cad31fd3a02c2a24a889b9e
wanzzhehe <wanzzmicro@outlook.com>
2022-06-13 09:19:49 +0800

    clang-format 增加自定义选择设置区间的功能

--- query.cpp
+++ query.cpp
@@ -5,30 +5,37 @@
 
 using boost::asio::ip::tcp;
 
-int main(int argc, char* argv[]) {
+int main(int argc, char *argv[]) {
     if (argc != 2) {
         std::cout << "Usage : " << argv[0] << " <Host>" << std::endl;
         return -1;
     }
 
-
     boost::asio::io_context io_context;
-    tcp::resolver resolver{ io_context };
+    tcp::resolver resolver{io_context};
 
     auto endpoints = resolver.resolve(argv[1], "233");
 
-    tcp::socket socket{ io_context };
+    tcp::socket socket{io_context};
     boost::asio::connect(socket, endpoints);
 
     {
-        std::array<int, 2> read_buffer;
-        size_t len = boost::asio::read(
-            socket, boost::asio::buffer(read_buffer));
-        if (len != sizeof(int) * 2) {
-            std::cerr << "Unknown range." << std::endl;
-            return 1;
+        std::array<int, 2> range;
+        std::string str;
+
+        std::cout << "是否自定义范围？ [y/N]";
+        std::getline(std::cin, str);
+        if (tolower(str[0]) != 'y') {
+            range[0] = 0, range[1] = -1;
+        } else {
+            std::cout << "输入范围，用空格隔开：";
+            std::cin >> range[0] >> range[1];
         }
-        std::cout << "猜数的范围为：" << read_buffer[0] << ' ' << read_buffer[1] << '\n';
+        boost::asio::write(socket, boost::asio::buffer(range));
+
+        boost::asio::read(socket, boost::asio::buffer(range));
+        std::cout << "实际猜数的范围为：" << range[0] << ' ' << range[1]
+                  << '\n';
     }
 
     for (;;) {
@@ -43,17 +50,13 @@ int main(int argc, char* argv[]) {
         if (t == 0) {
             std::cout << "哇，你猜对了！" << std::endl;
             break;
-        }
-        else if (t < -10) {
+        } else if (t < -10) {
             std::cout << "太小了。" << std::endl;
-        }
-        else if (t < 0) {
+        } else if (t < 0) {
             std::cout << "比较接近，但还是小了。" << std::endl;
-        }
-        else if (t > 10) {
+        } else if (t > 10) {
             std::cout << "太大了。" << std::endl;
-        }
-        else {
+        } else {
             std::cout << "比较接近，但还是大了。" << std::endl;
         }
     }
--- server.cpp
+++ server.cpp
@@ -1,84 +1,86 @@
 #include <boost/asio.hpp>
 
+#include <functional>
 #include <iostream>
 #include <memory>
 #include <random>
-#include <functional>
 
 using boost::asio::ip::tcp;
 
 class tcp_connection : public std::enable_shared_from_this<tcp_connection> {
 public:
     using pointer = std::shared_ptr<tcp_connection>;
-    static pointer create(boost::asio::io_context& io) {
+    static pointer create(boost::asio::io_context &io) {
         return pointer(new tcp_connection(io));
     }
 
-    tcp::socket& socket() { return m_socket; }
+    tcp::socket &socket() { return m_socket; }
     void start() {
-        std::array<int, 2> buffer{ 1, 1000 };
-        boost::asio::async_write(
-            m_socket, boost::asio::buffer(buffer),
-            std::bind(&tcp_connection::handle_start, shared_from_this(),
-                std::placeholders::_1,
-                std::placeholders::_2));
+        boost::asio::async_read(
+            m_socket, boost::asio::buffer(range),
+            [this, self = shared_from_this()](
+                const boost::system::error_code &ec, size_t) {
+                if (range[0] == 0 && range[1] == -1) {
+                    range = {1, 1000};
+                }
+                boost::asio::async_write(
+                    m_socket, boost::asio::buffer(range),
+                    std::bind(&tcp_connection::handle_start, shared_from_this(),
+                              std::placeholders::_1, std::placeholders::_2));
+            });
     }
 
 private:
-    tcp_connection(boost::asio::io_context& io) : m_socket(io) {}
+    tcp_connection(boost::asio::io_context &io) : m_socket(io) {}
 
-    void handle_start(const boost::system::error_code& /*ec*/,
-        size_t /*bytes_transferred*/) {
+    void handle_start(const boost::system::error_code & /*ec*/,
+                      size_t /*bytes_transferred*/) {
         static std::random_device rd;
-        static std::default_random_engine rng{ rd() };
-        static std::uniform_int_distribution<unsigned> u(1, 1000);
+        static std::default_random_engine rng{rd()};
+        static std::uniform_int_distribution<int> u(range[0], range[1]);
         m_number = u(rng);
 
         std::cout << "New Player from " << m_socket.remote_endpoint().address()
-            << ':' << m_socket.remote_endpoint().port()
-            << " be in this server. The answer is " << m_number
-            << std::endl;
+                  << ':' << m_socket.remote_endpoint().port()
+                  << " be in this server. The answer is " << m_number
+                  << ". The range is [" << range[0] << ", " << range[1] << "]"
+                  << std::endl;
 
         boost::asio::async_read(
             m_socket, boost::asio::buffer(buffer),
             std::bind(&tcp_connection::handle_guess, shared_from_this(),
-                std::placeholders::_1,
-                std::placeholders::_2));
+                      std::placeholders::_1, std::placeholders::_2));
     }
 
-    void handle_guess(const boost::system::error_code& ec,
-        size_t /*bytes_transferred*/) {
+    void handle_guess(const boost::system::error_code &ec,
+                      size_t /*bytes_transferred*/) {
         if (ec) {
             std::cerr << ec.message() << std::endl;
             return;
         }
-        int number;
-        memcpy(std::addressof(number), buffer.data(), sizeof(int));
+        int number = buffer[0];
         int ret = (number - m_number);
         memcpy(buffer.data(), std::addressof(ret), sizeof(int));
 
         std::cout << "Player from " << m_socket.remote_endpoint().address()
-            << ':' << m_socket.remote_endpoint().port() << " guesses "
-            << number << std::endl;
+                  << ':' << m_socket.remote_endpoint().port() << " guesses "
+                  << number << std::endl;
 
         if (number == m_number) {
             boost::asio::async_write(
                 m_socket, boost::asio::buffer(buffer),
                 std::bind(&tcp_connection::handle_success, shared_from_this(),
-                    std::placeholders::_1,
-                    std::placeholders::_2));
-        }
-        else {
+                          std::placeholders::_1, std::placeholders::_2));
+        } else {
             boost::asio::async_write(
                 m_socket, boost::asio::buffer(buffer),
                 std::bind(&tcp_connection::handle_report, shared_from_this(),
-                    std::placeholders::_1,
-                    std::placeholders::_2));
+                          std::placeholders::_1, std::placeholders::_2));
         }
     }
 
-    void handle_report(const boost::system::error_code& ec,
-        size_t /*bytes_transferred*/) {
+    void handle_report(const boost::system::error_code &ec,
+                       size_t /*bytes_transferred*/) {
         if (ec) {
             std::cerr << ec.message() << " [" << ec << "]" << std::endl;
             return;
@@ -86,26 +88,26 @@ private:
         boost::asio::async_read(
             m_socket, boost::asio::buffer(buffer),
             std::bind(&tcp_connection::handle_guess, shared_from_this(),
-                std::placeholders::_1,
-                std::placeholders::_2));
+                      std::placeholders::_1, std::placeholders::_2));
     }
 
     void handle_success(const boost::system::error_code /*ec*/,
-        size_t /*bytes_transferred*/) {
+                        size_t /*bytes_transferred*/) {
         std::cout << "Player from " << m_socket.remote_endpoint().address()
-            << ':' << m_socket.remote_endpoint().port() << " wines "
-            << std::endl;
+                  << ':' << m_socket.remote_endpoint().port() << " wines "
+                  << std::endl;
     }
 
     tcp::socket m_socket;
     int m_number;
-    std::array<char, sizeof(int)> buffer;
+    std::array<int, 1> buffer;
+    std::array<int, 2> range{1, 1000};
 };
 
 class tcp_server {
 public:
-    tcp_server(boost::asio::io_context& io)
-        : m_io(io), m_acceptor{ io, tcp::endpoint(tcp::v4(), 233) } {
+    tcp_server(boost::asio::io_context &io)
+        : m_io(io), m_acceptor{io, tcp::endpoint(tcp::v4(), 233)} {
         start_accept();
     }
 
@@ -115,28 +117,27 @@ private:
         m_acceptor.async_accept(
             new_connection->socket(),
             std::bind(&tcp_server::handle_accept, this, new_connection,
-                std::placeholders::_1));
+                      std::placeholders::_1));
     }
 
     void handle_accept(tcp_connection::pointer new_connection,
-        const boost::system::error_code& ec) {
+                       const boost::system::error_code &ec) {
         if (!ec) {
             new_connection->start();
         }
         start_accept();
     }
 
-    boost::asio::io_context& m_io;
+    boost::asio::io_context &m_io;
     tcp::acceptor m_acceptor;
 };
 
 int main() {
     try {
         boost::asio::io_context io;
-        tcp_server server{ io };
+        tcp_server server{io};
         io.run();
-    }
-    catch (std::exception& e) {
+    } catch (std::exception &e) {
         std::cerr << e.what() << std::endl;
     }
 }
